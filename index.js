58f74e70e2828437380557ae89853cb884d6a965
Shane Vincent <theshanergy@gmail.com>
2020-10-24 13:12:00 -0700

    add conversation history, allow user to choose enabled chats via inquirer, set names programatically

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const fs = require('fs')
 const axios = require('axios')
+const inquirer = require('inquirer')
 
 const { Client } = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
@@ -12,6 +13,9 @@ let sessionCfg
 if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH)
 }
+
+// Create array of selected contacts.
+let selectedContacts = []
 
 // Instantiate new WhatsApp client.
 const client = new Client({ session: sessionCfg })
@@ -34,69 +38,107 @@ client.on('authenticated', (session) => {
 
 // On auth failure.
 client.on('auth_failure', message => {
-    // Fired if session restore was unsuccessfull
     console.error('AUTHENTICATION FAILURE', message)
 })
 
-// On ready.
-client.on('ready', () => {
+// On client ready.
+client.on('ready', async () => {
     console.log('Client is ready!')
 
-
+    // Choose enabled contacts.
+    chooseContacts()
 })
 
+// Prompt user to select active contacts.
+const chooseContacts = () => {
+    // Get list of current chat instances.
+    client.getChats().then((chats) => {
+        let contactChoices = []
+        // Loop through chats and build choices array.
+        chats.forEach((item, index) => {
+            if (index <= 5) {
+                contactChoices.push({ name: item.name, value: item.id._serialized })
+            }
+        })
+        inquirer
+            .prompt([
+                {
+                    type: 'checkbox',
+                    name: 'contacts',
+                    message: 'Select contacts',
+                    choices: contactChoices,
+                },
+            ])
+            .then(answers => {
+                // Set selected contacts array.
+                selectedContacts = answers.contacts
+            })
+            .catch(error => {
+                console.error('PROMPT FAILURE', error)
+            })
 
-// On message.
+    })
+}
+
+// On message received.
 client.on('message', async (message) => {
 
-    // Get contact name.
-    const contact = await message.getContact()
-    const contactName = contact.shortName
-
-    // Log message.
-    console.log(contactName + ': ' + message.body)
-
-
-    // Get Chat.
-    const chat = await message.getChat()
-
-    const history = await chat.fetchMessages({ limit: 10 })
-
-    console.log(history)
-
-    // Set up prompt.
-    let prompt = "The following is a conversation with AI. He is helpful, creative, clever, and very friendly.\n\n"
-
-    prompt += contactName + ": Hi, who are you?\n"
-    prompt += "AI: I'm AI, how's it going?\n"
-
-    prompt += contactName + ": " + message.body + "\n"
-    prompt += "AI:"
-
-    console.log(prompt)
-
-
-    // List of contacts who will interact with the AI.
-    let enabledContacts = [
-        
-        // 
-        // 
-    ]
-
     // If AI is enabled for this contact.
-    if (enabledContacts.includes(message.from)) {
+    if (selectedContacts.includes(message.from)) {
+
+        // Set my name.
+        const myName = client.info.pushname
+
+        // Get contact.
+        const contact = await message.getContact()
+
+        // Get contact name.
+        const contactName = contact.shortName
+
+        // Log message.
+        console.log(contactName + ': ' + message.body)
+
+        // Get Chat.
+        const chat = await message.getChat()
+
+        // Set up prompt.
+        let prompt = "The following is a conversation between " + myName + " and " + contactName + ". " + myName + " is a 33 year old man. He is helpful, creative, clever, and friendly.\n\n"
+
+        // Pre-train via prompt.
+        prompt += contactName + ": How are you?\n"
+        prompt += myName + ": I'm good, how are you?\n"
+
+        prompt += contactName + ": Good, what are you doing?\n"
+        prompt += myName + ": Just working on my computer\n"
+
+        prompt += contactName + ': How much does the earth weigh?\n'
+        prompt += myName + ': The earth weighs 13,170,000,000,000,000,000,000,000 pounds\n'
+
+        // Loop through last 10 messages of history.
+        const history = await chat.fetchMessages({ limit: 10 })
+        history.forEach(function (item, index) {
+            // Get author name
+            const name = item.from == message.from ? contactName : myName
+            // Add to prompt.
+            if (!prompt.includes(item.body)) {
+                prompt += name + ': ' + item.body + '\n'
+            }
+        })
+
+        // Finalize prompt.
+        prompt += myName + ':'
 
         // Set typing state.
         chat.sendStateTyping()
 
-
-        // Query GPT-3 API
+        // Query GPT-3 API.
         axios
             .post('https://api.openai.com/v1/engines/davinci/completions', {
                 prompt: prompt,
-                temperature: 0.9,
+                temperature: 0.6,
                 max_tokens: 100,
                 top_p: 1,
+                frequency_penalty: 0.5,
                 presence_penalty: 0.6,
                 stop: '\n',
             }, {
@@ -106,13 +148,13 @@ client.on('message', async (message) => {
                 },
             })
             .then((response) => {
+                let responseText = response.data.choices[0].text.trim()
                 // Send reply.
-                client.sendMessage(message.from, response.data.choices[0].text)
+                client.sendMessage(message.from, responseText)
                 // Log reply.
-                console.log('AI: ', response.data.choices[0].text)
+                console.log(myName + ': ', responseText)
             })
-            .catch((error) => console.log('BAD', error))
-
+            .catch((error) => console.error('GPT-3 REQUEST FAILURE', error))
 
     }
 })
